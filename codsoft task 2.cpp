d48d8fd279c730a32e705c99fee426b6c7a08247
vanshpaliwal13 <vanshpaliwal13@gmail.com>
2025-03-17 18:17:50 +0530

    Add files via upload

---
+++ codsoft task 2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2;
+    char op;
+
+    // User inputs two numbers and an operation
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << "Choose an operation (+, -, *, /): ";
+    cin >> op;
+
+    // Perform the operation based on the user's choice
+    switch(op) {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+            break;
+        case '/':
+            // Check for division by zero
+            if (num2 != 0) {
+                cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+            } else {
+                cout << "Error! Division by zero." << endl;
+            }
+            break;
+        default:
+            cout << "Invalid operator!" << endl;
+    }
+1
+    return 0;
+}
